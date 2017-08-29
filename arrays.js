8d02d49025f5a134a9f243d99e07dbd0f01011e6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 03:08:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+addElementToBeginningOfArray()
+destructivelyAddElementToBeginningOfArray()

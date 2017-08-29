be0bfd66c940971106cda1b8a5b1c7f34eba5a3e
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 03:12:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array]
   return newArray

cfd3de7814972bce02d4f3784477e1ffa62920f9
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 03:16:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,3 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-
-addElementToBeginningOfArray()
-destructivelyAddElementToBeginningOfArray()

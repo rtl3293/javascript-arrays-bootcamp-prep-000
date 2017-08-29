5e638bdb799dd62288a75aed7f6a321094c7c2d4
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 03:10:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,7 @@
-
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+  return newArray
+}
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
